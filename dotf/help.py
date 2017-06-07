1493bfa5ae1dcdcac5905e13053e8eda9242f334
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-06-07 12:18:42 +0100

    Add pytest + pytest-spec and configure it for spec like syntax

---
+++ dotf/help.py
@@ -0,0 +1,7 @@
+class Help():
+
+    """Outputs help for the various .f commands"""
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        pass
---
+++ spec/help_spec.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class SpecHelp(object):
+    def it_returns_help_text_for_dotf_command(self):
+        """TODO: to be defined. """
+        assert(True)
