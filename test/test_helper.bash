457c051afe738d8f35d9ce27538e4a374c1e4adc
Marco Lopes <marco.paulo.lopes@gmail.com>
2017-10-28 19:10:35 +0100

    Fix indentation issues

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -3,22 +3,22 @@ load 'test_helper/bats-assert/load'
 load 'test_helper/bats-file/load'
 
 fixtures () {
-    GIT_REPO_FIXTURE_PATH="$BATS_TEST_DIRNAME/fakerepo"
-    DOTF_PROJECT_ROOT="$(pwd)"
-    PROJECT_SOURCE="$DOTF_PROJECT_ROOT/src"
+  GIT_REPO_FIXTURE_PATH="$BATS_TEST_DIRNAME/fakerepo"
+  DOTF_PROJECT_ROOT="$(pwd)"
+  PROJECT_SOURCE="$DOTF_PROJECT_ROOT/src"
 }
 
 setup () {
-    export ORIGINAL_PATH=$PATH
-    export PATH=$PATH:$PROJECT_SOURCE
+  export ORIGINAL_PATH=$PATH
+  export PATH=$PATH:$PROJECT_SOURCE
 
-    GIT_REPO_FIXTURE_PATH=$(temp_make)
-	cd $GIT_REPO_FIXTURE_PATH
+  GIT_REPO_FIXTURE_PATH=$(temp_make)
+  cd $GIT_REPO_FIXTURE_PATH
 }
 
 teardown () {
-	cd $DOTF_PROJECT_ROOT
-    temp_del $GIT_REPO_FIXTURE_PATH
+  cd $DOTF_PROJECT_ROOT
+  temp_del $GIT_REPO_FIXTURE_PATH
 
-    export PATH=$ORIGINAL_PATH
+  export PATH=$ORIGINAL_PATH
 }
